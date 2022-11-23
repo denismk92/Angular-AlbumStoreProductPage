0224b850344fb5c5d01037ab015e5113619fcbc1
denismk92 <118901339+denismk92@users.noreply.github.com>
2022-11-23 16:10:43 +0200

    Small fixes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs';
-import 'rxjs/operator/map';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class ProductService {
@@ -16,6 +16,6 @@ export class ProductService {
   }
   
   getAlbum(id:number){
-   return this._http.get(this._albumUrl).map(response => response.json())
+   return this._http.get(this._albumUrl).map((response) => response.json())
   }
 }
